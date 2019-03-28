3800a0c44d12ea14e62da2c23a1b7d8e8e354ce7
Jim Fitzpatrick <jimfity@gmail.com>
2019-03-28 20:18:43 +0000

    Basic routes added

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const Hapi = require('hapi');
+const dotenv = require('dotenv');
+// require('./app/models/db');
+
+const result = dotenv.config();
+if(result.error){
+  console.log(result.error.message);
+  process.exit(1);
+}
+
+const server = Hapi.server({
+  port: process.env.PORT || 3000,
+});
+
+
+async function init() {
+  await server.register(require('hapi-auth-cookie'));
+
+  server.auth.strategy('standard', 'cookie', {
+    cookie: {
+      name: process.env.COOKIE_NAME,
+      password: process.env.COOKIE_PASSWORD,
+      isSecure: false
+    },
+
+    // ttl: 24*60*60*1000,
+    redirectTo: '/'
+  });
+
+  server.auth.default({
+    mode: 'required',
+    strategy: 'standard'
+  });
+
+  server.route(require('./routes'));
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+}
+
+process.on('unhandledRejection', err => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
